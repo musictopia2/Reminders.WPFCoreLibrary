2790370df849ccc1777942fbed0eed593a4eb38a
Andy Walter <musictopia2@hotmail.com>
2025-12-03 18:22:18 -0700

    Converted to using the new extension model in c#.

--- PopUps/BasicPopUp.cs
+++ PopUps/BasicPopUp.cs
@@ -14,7 +14,6 @@ public class BasicPopUp : IPopUp
     private LoadSoundsHelperClass? _sounds;
     public static Func<Task>? CloseCustomToastAsync { get; set; }
     public static bool IsTesting { get; set; } = false;
-    public static bool ForceNoSound { get; set; } = false;
     private HiddenReminderWindow? _hidden;
     private SoundReminderWindow? _soundWindow;
     private readonly ReminderContainer _container;
@@ -36,10 +35,6 @@ public class BasicPopUp : IPopUp
     }
     public void PlaySound(int howOftenToRepeat)
     {
-        if (ForceNoSound)
-        {
-            return; //because forced no sound means do not even try.
-        }
         if (SupportsSound == false)
         {
             throw new CustomBasicException("Cannot play sounds because not even supported.");
@@ -49,10 +44,6 @@ public class BasicPopUp : IPopUp
     }
     private void CloseSoundPopup()
     {
-        if (ForceNoSound)
-        {
-            return; //because forced no sound means do not even try.
-        }
         if (_sounds is not null)
         {
             _sounds.StopPlay();
